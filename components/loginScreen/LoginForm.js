844d815a8ef14d47680cab0ae9c70ca6346d63eb
Somon Das <somondas067@gmail.com>
2022-10-06 15:51:34 -0700

    1. Almost complete the login form. 2. Found some new bugs 3. Form needs some fixes

--- components/loginScreen/LoginForm.js
+++ components/loginScreen/LoginForm.js
@@ -5,58 +5,78 @@ import { Pressable } from 'react-native'
 import { TouchableOpacity } from 'react-native'
 import { Formik } from 'formik'
 import * as Yup from "yup"
-import { Validator } from 'email-validator'
+import { Validator } from 'email-validator';
 // |                                                                                                 
 const LoginForm = () => {
     const LoginFormSchema = Yup.object().shape({
         email: Yup.string().email().required("An email is requred"),
-        password: Yup.string().required().min(8, "Password must contain at least 8 character")
+        password: Yup.string().required().min(6, "Password must contain at least 8 character")
     })
     return (
         <View style={styles.wrapper}>
             <Formik
-            initialValues={{email: "", password: "",}}
-            onSubmit={val =>{
-                console.log(val);
-            }}
-            validationSchema={LoginFormSchema}
-            validateOnMount={true}
+                initialValues={{ email: "", password: "", }}
+                onSubmit={values => {
+                    console.log(values);
+                }}
+                validationSchema={LoginFormSchema}
+                validateOnMount={true}
             >
-                <View style={styles.inputField}>
-                    <TextInput
-                        placeholderTextColor="#444"
-                        placeholder="Phone Number, username or email"
-                        autoCapitalize='none'
-                        keyboardType='email-address'
-                        textContentType='emailAddress'
-                        autoFocus={true}
-                    />
-                </View>
-                <View style={styles.inputField}>
-                    <TextInput
-                        placeholderTextColor="#444"
-                        placeholder="Password"
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                        secureTextEntry={true}
-                        keyboardType="password"
-                        textContentType='password'
-                    />
+                {({ handleBlur, handleChange, handleSubmit, values, isValid }) => (
 
-                </View>
-                <View style={{ alignItems: "flex-end", marginBottom: 30, }}>
-                    <Text style={{ color: "#6bb0f5" }}>Forgot Password?</Text>
-                </View>
-                <Pressable titleSize={20} style={styles.button}>
-                    <Text style={styles.buttonText}>Log In</Text>
-                </Pressable>
-                <View style={styles.signupContainer}>
+                    <>
+                        <View style={[styles.inputField,{
+                            borderColor: 1 > values.email.length || values.email.length >= 6 ? "#ccc": "red"
+                        }]}>
+                            <TextInput
+                                placeholderTextColor="#444"
+                                placeholder="Phone Number, username or email"
+                                autoCapitalize='none'
+                                keyboardType='email-address'
+                                textContentType='emailAddress'
+                                autoFocus={true}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                            />
+                        </View>
+                        <View style={[styles.inputField, 
+                        {borderColor: 1 > values.password.length || values.password.length >= 6 ? "#ccc": "red"}
+                        ]}>
+                            <TextInput
+                                placeholderTextColor="#444"
+                                placeholder="Password"
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                                secureTextEntry={true}
+                                keyboardType="password"
+                                textContentType='password'
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                            />
 
-                    <Text>Don't have an account?</Text>
-                    <TouchableOpacity>
-                        <Text style={{ color: "#6bb0f5" }}>Sign In</Text>
-                    </TouchableOpacity>
-                </View>
+                        </View>
+                        <View style={{ alignItems: "flex-end", marginBottom: 30, }}>
+                            <Text style={{ color: "#6bb0f5" }}>Forgot Password?</Text>
+                        </View>
+                        <Pressable
+                            onPress={handleSubmit}
+                            titleSize={20}
+                            style={styles.button(isValid)}
+                            disabled={!isValid}
+                            >
+                            <Text style={styles.buttonText}>Log In</Text>
+                        </Pressable>
+                        <View style={styles.signupContainer}>
+
+                            <Text>Don't have an account?</Text>
+                            <TouchableOpacity>
+                                <Text style={{ color: "#6bb0f5" }}>Sign In</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                )}
             </Formik>
         </View>
     )
@@ -73,17 +93,17 @@ const styles = StyleSheet.create({
         backgroundColor: "#fafafa",
         marginBottom: 10,
         borderWidth: .5,
-        borderColor: "grey"
+        borderColor: "#ccc"
 
     },
-    button: {
-        backgroundColor: "#0096f6",
+    button: isValid =>({
+        backgroundColor: isValid ? "#0096f6": "#9acaf7",
         minHeight: 12,
         alignItems: "center",
         justifyContent: "center",
         padding: 12,
         borderRadius: 4,
-    },
+    }),
     buttonText: {
         fontSize: 16,
         color: "#fff",
